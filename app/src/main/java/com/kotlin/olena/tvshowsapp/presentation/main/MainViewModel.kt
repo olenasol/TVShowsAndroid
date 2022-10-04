dc89423f2a5e1c0df1a27c2ec2197d95a507dac7
Olena Soluk <olenasoluk@ukr.net>
2022-10-04 16:50:52 +0300

    switched to authstate to hot flow

--- app/src/main/java/com/kotlin/olena/tvshowsapp/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/kotlin/olena/tvshowsapp/presentation/main/MainViewModel.kt
@@ -1,12 +1,19 @@
 package com.kotlin.olena.tvshowsapp.presentation.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.kotlin.olena.tvshowsapp.domain.usecase.AuthStateUseCase
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.shareIn
 import javax.inject.Inject
 
 class MainViewModel @Inject constructor(
     private val authStateUseCase: AuthStateUseCase
 ): ViewModel() {
 
-    val authStateFlow = authStateUseCase.getAuthStateFlow()
+    val authStateFlow = authStateUseCase.getAuthStateFlow().shareIn(
+        viewModelScope,
+        SharingStarted.WhileSubscribed(),
+        1
+    )
 }
